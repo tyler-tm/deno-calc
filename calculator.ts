f7f7a9705c851cc4d38b6546c313f65cd30b1bb7
Tyler M <tyler-tm@protonmail.com>
2020-07-26 22:31:39 -0400

    Refactors calculator, using function composition for calculate

--- calculator.ts
+++ calculator.ts
@@ -1,13 +1,14 @@
+import compose from "https://deno.land/x/denofun/compose.ts";
 import { tokenize } from "./lexer.ts";
 import { parseList } from "./parser.ts";
 import { traverse } from "./traverser.ts";
 
 const input = Deno.args.join("");
 
-export const calculate = (input: string): number => {
-  const tokenHead = tokenize(input); // tokenize into a singly-linked list
-  const ast = parseList(tokenHead); // parse token list into an abstract syntax tree
-  return traverse(ast); // postorder traversal of AST to calculate result
-};
+export const calculate: (input: string) => number = compose(
+  traverse,
+  parseList,
+  tokenize,
+);
 
 console.log(calculate(input));
